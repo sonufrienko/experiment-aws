55346dd2232927e33e10c155845dd39db995d91a
Sergey Onufrienko <sergey@onufrienko.com>
2020-12-25 19:55:17 +0200

    Remove

--- api-sqs-async-processing/bin/api-sqs-async-processing.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ApiSqsAsyncProcessingStack } from '../lib/api-sqs-async-processing-stack';
-
-const app = new cdk.App();
-new ApiSqsAsyncProcessingStack(app, 'ApiSqsAsyncProcessingStack');
