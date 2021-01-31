2970e24b67bbe542c373f717b0309b2aba9b2992
Sergey Onufrienko <sergey@onufrienko.com>
2021-01-31 22:35:17 +0200

    Update stacks

--- ecs/infrastructure/bin/infrastructure.ts
+++ ecs/infrastructure/bin/infrastructure.ts
@@ -3,23 +3,12 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { EcrStack } from '../lib/ecr-stack';
 import { EcsStack } from '../lib/ecs-stack';
-import { PipelineStack } from '../lib/pipeline-stack';
-import { ServiceAttributes } from '../lib/types';
 
 const app = new cdk.App();
-const services: ServiceAttributes[] = [
-  {
-    name: 'service-js',
-    healthCheckPath: '/test',
-    port: 4000,
-  },
-  {
-    name: 'service-py',
-    healthCheckPath: '/py/test',
-    port: 80,
-  },
-];
 
-new EcrStack(app, 'ecr-stack', { services });
-new EcsStack(app, 'ecs-stack', { services });
-new PipelineStack(app, 'pipeline-stack');
+const serviceJsEcr = new EcrStack(app, 'ecr-service-js', { repositoryName: 'service-js' });
+const servicePyEcr = new EcrStack(app, 'ecr-service-py', { repositoryName: 'service-py' });
+const ecs = new EcsStack(app, 'ecs', {
+  serviceJsRepository: serviceJsEcr.repository,
+  servicePyRepository: servicePyEcr.repository,
+});
